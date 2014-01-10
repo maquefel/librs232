f562da47429f95871412b68388c886f2ffd9c37c
speakinghedge <hecke@naberius.de>
2014-01-10 22:46:51 +0100

    posix: add missing free in rs232_init

--- src/rs232_posix.c
+++ src/rs232_posix.c
@@ -48,8 +48,10 @@ rs232_init(void)
 		return NULL;
 
 	p->pt = (struct rs232_posix_t *) malloc(sizeof(struct rs232_posix_t));
-	if (p->pt == NULL)
+	if (p->pt == NULL) {
+		free(p);
 		return NULL;
+	}
 
 	DBG("p=%p p->pt=%p\n", (void *)p, p->pt);
 
